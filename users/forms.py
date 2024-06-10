ec51ce9736db967d9e2ab396eb7c096dc98882e5
Kunjesh <kunjpanchal2414@gmail.com>
2024-06-10 20:56:51 +0530

    Profile Update Feature

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,7 @@
 from django import forms
-
 from django.contrib.auth.models import User
-
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -10,3 +9,17 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    #  allows to update user fields such as username and email
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+        
+class ProfileUpdateForm(forms.ModelForm):
+    #  allows to update profile field such as profile image
+    class Meta:
+        model = Profile
+        fields = ['image']
